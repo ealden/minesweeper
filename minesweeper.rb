50002c70bae4a8e99305e603a17ca28ce6ef6625
Ealden Esto E. Escañan <ealden@escanan.com>
2019-07-25 00:01:51 +0800

    Add mines to the minefield at... #add_mine

--- minesweeper.rb
+++ minesweeper.rb
@@ -18,18 +18,14 @@ class Minesweeper
 
   def add_mine column:, row:
     @mines << { column: column, row: row }
+
+    x = row - 1
+    y = column - 1
+
+    @minefield[x][y] = MINE
   end
 
   def sweep
-    minefield = @minefield
-
-    @mines.each do |mine|
-      x = mine[:row] - 1
-      y = mine[:column] - 1
-
-      minefield[x][y] = MINE
-    end
-
-    minefield
+    @minefield
   end
 end
